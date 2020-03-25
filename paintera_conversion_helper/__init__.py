84da2d0f7e340ae9468e06b5732ba7d6a35b7f1f
Igor Pisarev <pisarevi@janelia.hhmi.org>
2020-03-25 13:07:49 -0400

    remove old paintera-conversion-helper command, use paintera-convert

--- paintera_conversion_helper/__init__.py
+++ paintera_conversion_helper/__init__.py
@@ -5,7 +5,6 @@ import warnings
 
 from . import version
 
-_paintera_conversion_helper = '@CommandLineConverter'
 _paintera_convert           = '@PainteraConvert'
 _extract_to_scalar          = '@ExtractHighestResolutionLabelDataset'
 _groupId                    = 'org.janelia.saalfeldlab'
@@ -25,19 +24,6 @@ def _add_spark_master(argv, spark_master):
     if double_dash_index == -1:
         spark_master_arg += ['--']
     return  spark_master_arg + argv
-
-def launch_pch(argv=sys.argv[1:]):
-    try:
-        return jgo.util.main_from_endpoint(
-            primary_endpoint            = f'{_groupId}:{_artifactId}',
-            primary_endpoint_version    = version._paintera_conversion_helper_version.maven_version(),
-            primary_endpoint_main_class = _paintera_conversion_helper,
-            argv                        = _add_spark_master(argv, os.getenv('SPARK_MASTER', 'local[*]')))
-    finally:
-        warnings.warn(
-            'The paintera-conversion-helper command has been deprecated. Please use paintera-convert instead.',
-            DeprecationWarning,
-            stacklevel=2)
 
 def launch_extract_to_scalar(argv=sys.argv[1:]):
     try:
